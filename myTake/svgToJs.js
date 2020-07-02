ea46cd3e022b26cbb08b47297c95fe61958bb974
Mewen <mewen25@gmail.com>
2020-07-02 11:52:19 +0100

    improved

--- myTake/svgToJs.js
+++ myTake/svgToJs.js
@@ -1,116 +1,124 @@
-const path = require('path')
-const fs = require('fs')
-const svgson = require('svgson')
+const path = require("path");
+const fs = require("fs");
+const svgson = require("svgson");
 
 /**
  * Convert SVG file into JS file
- * 
+ *
  * @param {string} dir - Name of directory
  * @param {string} file - Name of SVG file
  */
 const svgToJs = (file) => {
-  fs.mkdir(path.join(__dirname, 'maps', file.slice(0, file.length-4)), (err) => { 
-    if (err) { 
-        return console.error(err); 
-    } 
-    console.log('Directory created successfully!'); 
-  }); 
-  const svgFile = path.join(__dirname, 'maps', file)
-  const jsFile = path.join(__dirname, 'maps', file.slice(0, file.length-4), 'mapData.js')
+	fs.mkdir(
+		path.join(__dirname, "maps", file.slice(0, file.length - 4)),
+		(err) => {
+			if (err) {
+				return console.error(err);
+			}
+			console.log("Directory created successfully!");
+		}
+	);
+	const svgFile = path.join(__dirname, "maps", file);
+	const jsFile = path.join(
+		__dirname,
+		"maps",
+		file.slice(0, file.length - 4),
+		"svgData.js"
+	);
 
-  if (fs.existsSync(jsFile)) {
-    console.log(`File ${jsFile} already exists`)
-    return
-  }
+	if (fs.existsSync(jsFile)) {
+		console.log(`File ${jsFile} already exists`);
+		return;
+	}
 
-  const handlePath = (element) => {
-    const id = element.attributes.id;
-    const obj = {
-      [id]: {
-        id: id,
-        d: [element.attributes.d]
-      }
-    }
-    console.log("path:", obj);
-    return obj;
-  }
+	const handlePath = (element) => {
+		const id = element.attributes.id;
+		const obj = {
+			[id]: {
+				id: id,
+				d: [element.attributes.d],
+			},
+		};
+		console.log("path:", obj);
+		return obj;
+	};
 
-  const handleGroup = (element) => {
-    const id = element.attributes.id;
-    const paths = element.children.map(e => e.attributes.d);
-    const obj = {
-      [id]: {
-        id: id,
-        d: [paths]
-      }
-    }
-    console.log("groups:", obj);
-    return obj;
-  }
+	const handleGroup = (element) => {
+		const id = element.attributes.id;
+		const paths = element.children.map((e) => e.attributes.d);
+		const others = element.children.filter((e) => e.name !== "path");
+		console.log("others found", others);
+		const obj = {
+			[id]: {
+				id: id,
+				paths: [paths],
+				others: [others],
+			},
+		};
+		console.log("groups:", obj);
+		return obj;
+	};
 
-  fs.readFile(svgFile, 'utf8', (err, data) => {
-    if (err) {
-      console.error(`Unable to read file ${svgFile}`, err)
-      return
-    }
-    console.log(`Parsing file ${svgFile}`)
-    svgson.parse(data)
-      .then(json => {
+	fs.readFile(svgFile, "utf8", (err, data) => {
+		if (err) {
+			console.error(`Unable to read file ${svgFile}`, err);
+			return;
+		}
+		console.log(`Parsing file ${svgFile}`);
+		svgson
+			.parse(data)
+			.then((json) => {
+				const obj = {
+					label: json.attributes["aria-label"],
+					viewBox: json.attributes.viewBox,
+					elements: json.children.map((element) =>
+						element.name === "path" ? handlePath(element) : handleGroup(element)
+					),
+				};
 
-        const obj = {
-          label: json.attributes['aria-label'],
-          viewBox: json.attributes.viewBox,
-          elements: json.children.map(element => element.name === "path" ? handlePath(element) : handleGroup(element))
-        }
+				// const obj = {
+				//   label: json.attributes['aria-label'],
+				//   viewBox: json.attributes.viewBox,
+				//   locations: json.children
+				//     .filter(child => {
+				//       if (child.name !== 'path') {
+				//         console.warn(`<${child.name}> tag will be ignored`)
+				//         return false
+				//       }
 
+				//       return true
+				//     })
+				//     .map(child => ({
+				//       name: child.attributes.name,
+				//       id: child.attributes.id,
+				//       path: child.attributes.d,
+				//     }))
+				// }
+				const js = `export default ${JSON.stringify(obj)}`;
 
-        // const obj = {
-        //   label: json.attributes['aria-label'],
-        //   viewBox: json.attributes.viewBox,
-        //   locations: json.children
-        //     .filter(child => {
-        //       if (child.name !== 'path') {
-        //         console.warn(`<${child.name}> tag will be ignored`)
-        //         return false
-        //       }
-
-        //       return true
-        //     })
-        //     .map(child => ({
-        //       name: child.attributes.name,
-        //       id: child.attributes.id,
-        //       path: child.attributes.d,
-        //     }))
-        // }
-        const js = `export default ${JSON.stringify(obj)}`
-
-        console.log(`Writing file ${jsFile}`)
-        fs.writeFile(jsFile, js, 'utf8', err => {
-          if (err) {
-            console.error(`Unable to write file ${jsFile}`, err)
-            return
-          }
-        })
-      }).catch(err => {
-        console.error(`Unable ton parse file ${svgFile}`, err)
-      })
-  })
-}
+				console.log(`Writing file ${jsFile}`);
+				fs.writeFile(jsFile, js, "utf8", (err) => {
+					if (err) {
+						console.error(`Unable to write file ${jsFile}`, err);
+						return;
+					}
+				});
+			})
+			.catch((err) => {
+				console.error(`Unable ton parse file ${svgFile}`, err);
+			});
+	});
+};
 
 // Read maps directory
-fs.readdir(path.join(__dirname, 'maps'), (err, files) => {
-  if (err) {
-    console.log('Unable to scan maps directory', err)
-    return
-  }
-  files.forEach(file => {
-    if (path.extname(file) === '.svg') {
-      svgToJs(file)
-    }
-  })
-})
-
-
-
-
-
+fs.readdir(path.join(__dirname, "maps"), (err, files) => {
+	if (err) {
+		console.log("Unable to scan maps directory", err);
+		return;
+	}
+	files.forEach((file) => {
+		if (path.extname(file) === ".svg") {
+			svgToJs(file);
+		}
+	});
+});
