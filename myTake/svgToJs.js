3b5ef5492b6f7c3fd717a64c64e7fd8d63b55cf5
mewen25 <mewen25@users.noreply.github.com>
2020-08-06 17:11:55 +0100

    updated

--- myTake/svgToJs.js
+++ myTake/svgToJs.js
@@ -43,20 +43,76 @@ const svgToJs = (file) => {
 		return obj;
 	};
 
+	const getPath = (element) => {
+		let path = "";
+		if (element.name === "path") {
+			path = element.attributes.d;
+		} else if (element.name === "rect") {
+		}
+		return path;
+	};
+
 	const handleGroup = (element) => {
 		const id = element.attributes.id;
-		const paths = element.children.map((e) => e.attributes.d);
+		const paths = element.children.map((e) => {
+			const ele = e;
+			const path = getPath(e);
+			console.log("ELEMENT", ele, "PATH/POS?", path);
+			if (path) return path;
+		});
+		console.log("group paths", paths, element.children);
 		const others = element.children.filter((e) => e.name !== "path");
 		console.log("others found", others);
 		const obj = {
-			[id]: {
-				id: id,
-				paths: [paths],
-				others: [others],
-			},
+			id: id,
+			paths: [paths],
+			others: [others],
 		};
 		console.log("groups:", obj);
 		return obj;
+	};
+
+	const getElement = (e) => {
+		const id = e.attributes.id;
+		var renders = [];
+		var render = {};
+		console.log("ELEMENTSSSS", e);
+		if (e.name === "path") render = [{type: e.name, attributes: {d: e.attributes.d}} ];
+		else if (e.name === "g") {
+			render = e.children.map((c) => {
+				console.log("ATTRIBUTES", c.attributes);
+				return c.name === "rect"
+					? {
+							type: c.name,
+							attributes: {
+								x: c.attributes.x,
+								y: c.attributes.y,
+								width: c.attributes.width,
+								height: c.attributes.height,
+								style: c.attributes.style,
+							},
+					  }
+					: c.name === "ellipse" ? {
+						type: "circle",
+						attributes: {
+							cx: c.attributes.cx,
+							cy: c.attributes.cy,
+							rx: c.attributes.rx,
+							ry: c.attributes.ry,
+							style: c.attributes.style
+						}
+					} : { type: c.name, attributes: { d: c.attributes.d } };
+			});
+		}
+		console.log("RENDER", render);
+		renders.push(render);
+
+		let elementObj = {
+			id: id,
+			render: render,
+		};
+		console.log("ELEMENT", elementObj);
+		return elementObj;
 	};
 
 	fs.readFile(svgFile, "utf8", (err, data) => {
@@ -71,8 +127,9 @@ const svgToJs = (file) => {
 				const obj = {
 					label: json.attributes["aria-label"],
 					viewBox: json.attributes.viewBox,
-					elements: json.children.map((element) =>
-						element.name === "path" ? handlePath(element) : handleGroup(element)
+					elements: json.children.map(
+						(element) => getElement(element)
+						// element.name === "path" ? handlePath(element) : handleGroup(element)
 					),
 				};
 
